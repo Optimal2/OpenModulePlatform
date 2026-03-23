8557c1fab731f32df6a64999df4fc1cccaeafeab
GrillexN <58868271+GrillexN@users.noreply.github.com>
2026-03-23 20:12:00 +0100

    Added first, unpolished, version of topbar

--- OpenModulePlatform.Web.Shared/Options/WebAppOptions.cs
+++ OpenModulePlatform.Web.Shared/Options/WebAppOptions.cs
@@ -7,6 +7,20 @@ public enum PermissionMode
     All = 1
 }
 
+public sealed class PortalTopBarOptions
+{
+    public bool Enabled { get; set; }
+    public string PortalBaseUrl { get; set; } = "/";
+    public int VisibleLinkCount { get; set; } = 5;
+    public PortalTopBarLinkOptions[] Links { get; set; } = [];
+}
+
+public sealed class PortalTopBarLinkOptions
+{
+    public string TextKey { get; set; } = "";
+    public string Href { get; set; } = "";
+}
+
 public sealed class WebAppOptions
 {
     public const string DefaultSectionName = "WebApp";
@@ -14,6 +28,7 @@ public sealed class WebAppOptions
     public string Title { get; set; } = "OpenModulePlatform";
     public string DefaultCulture { get; set; } = "sv-SE";
     public string[] SupportedCultures { get; set; } = ["sv-SE", "en-US"];
+    public PortalTopBarOptions PortalTopBar { get; set; } = new();
     public bool AllowAnonymous { get; set; }
     public bool UseForwardedHeaders { get; set; }
     public PermissionMode PermissionMode { get; set; } = PermissionMode.Any;
