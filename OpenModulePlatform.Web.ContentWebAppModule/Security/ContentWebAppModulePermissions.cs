c0b1ea02992d0bc1da888a0770e3f4a7fd48e2ca
GrillexN <58868271+GrillexN@users.noreply.github.com>
2026-05-11 16:05:14 +0200

    Implement content web app CMS

--- OpenModulePlatform.Web.ContentWebAppModule/Security/ContentWebAppModulePermissions.cs
+++ OpenModulePlatform.Web.ContentWebAppModule/Security/ContentWebAppModulePermissions.cs
@@ -3,6 +3,5 @@ namespace OpenModulePlatform.Web.ContentWebAppModule.Security;
 
 public static class ContentWebAppModulePermissions
 {
-    public const string View = "ContentWebAppModule.View";
     public const string Manage = "ContentWebAppModule.Manage";
 }
